8dfb1557ab14f7aec7383bd2bac65b631ae098ba
Mauricio Arroyo <mauricioarroyo@ascbomacmini76.local>
2025-05-02 18:46:49 -0400

    added transaction

--- src/components/TransactionControl.jsx
+++ src/components/TransactionControl.jsx
@@ -1,15 +1,40 @@
+import { useEffect, useState } from "react";
 import styles from "../styles/TransactionControl.module.css";
+import { getTransactions } from "../services/transactionService";
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "short",
+  });
+}
 
 export default function TransactionControl({
   className,
   onAddTransactionClick,
+  onEditButtonClick,
 }) {
+  const [transactions, setTransactions] = useState([]);
+
+  async function fetchTransactions() {
+    const transactions = await getTransactions();
+    setTransactions(transactions);
+  }
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
   return (
     <div className={`${styles.container} ${className}`}>
       <div className={styles.headerSection}>
         <div className={styles.headerControls}>
           <div className={styles.addControls}>
-            <button className={styles.addButton} onClick={onAddTransactionClick} >
+            <button
+              className={styles.addButton}
+              onClick={onAddTransactionClick}
+            >
               <span className={`material-symbols-outlined ${styles.addIcon}`}>
                 add
               </span>
@@ -42,9 +67,31 @@ export default function TransactionControl({
       </div>
       <div className={styles.footerSection}>
         <div className={styles.transactionsListContainer}>
-          <div className={styles.transactionItem}>item1</div>
-          <div className={styles.transactionItem}>item1</div>
-          <div className={styles.transactionItem}>item1</div>
+          {transactions.map((transaction) => (
+            <div key={transaction.id} className={styles.transactionItem}>
+              <div className={styles.itemLeftSide}>
+                <span className={styles.transactionDate}>
+                  {formatDate(transaction.date)}{" "}
+                </span>
+                <span className={styles.transactionAccount}>
+                  cuenta banco fassil deah static
+                </span>
+                <span className={styles.transactionIcon}>-</span>
+                <span className={styles.transactionNote}>
+                  {transaction.note}
+                </span>
+              </div>
+              <div className={styles.itemRightSide}>
+                <span className={styles.amount}>{transaction.amount}</span>
+                <span
+                  className={`material-symbols-outlined ${styles.editIcon}`}
+                  onClick={() => onEditButtonClick(transaction)}
+                >
+                  edit
+                </span>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
+import { Fragment, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/globals.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <Fragment>
     <App />
-  </StrictMode>
+  </Fragment>
 );
